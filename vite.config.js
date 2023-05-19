f85a32df175cdee14045abbf9b45a01467aa77fb
Gabriel Yagoub <gabrielyagoub@gmail.com>
2023-05-19 15:53:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 module.exports = {
-  publicPath: 'https://gabriel-yagoub.github.io/vue-js-notesapp/'
+  publicPath: '/vue-js-notesapp/'
 }
 
 // https://vitejs.dev/config/
