773258eb8123f00bc45d635e978d4570c46be3b4
Gabriel Yagoub <gabrielyagoub@gmail.com>
2023-05-19 15:59:45 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-module.exports = {
-  publicPath: '/vue-js-notesapp/'
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
